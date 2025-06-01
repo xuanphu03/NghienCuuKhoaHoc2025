80d0be553d1607460e1f59aba07b5214a8903286
Xuan Phu <xuanphu1024@gmail.com>
2025-06-01 11:12:06 +0700

    feat(UI): refactor UI so colorful

--- src/components/hooks/getData.ts
+++ src/components/hooks/getData.ts
@@ -1,8 +1,14 @@
 import { database } from '@/lib/firebase';
 import { ref, onValue  } from 'firebase/database';
-
-
-export function getData(path: string, callback: (data: any) => void) {
+interface DataSensorProps {
+  doam: number | undefined;
+  luongMua: number | undefined;
+  nguy_co_sat_lo: number | undefined;
+  goc_do: number | undefined;
+  soLanGauLat: number | undefined;
+  soLanRung10Phut: number | undefined;
+}
+export function getData(path: string, callback: (data: DataSensorProps) => void) {
   try {
     const dbRef = ref(database, path);
 
